07c0854333d4cc1a3193d5311ada6c4b8ad136da
AKT-Physics <a.k.thomasson@gmail.com>
2025-10-09 08:34:56 +0100

    Update submit_mpi_evolution_2HDM_Z2_SLURM.sh

--- submit_mpi_evolution_2HDM_Z2_SLURM.sh
+++ submit_mpi_evolution_2HDM_Z2_SLURM.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=3D_scaling_1
-#SBATCH --output=/home/athomas/out_3D_scaling.txt
+#SBATCH --job-name=2HDM
+#SBATCH --output=/home/USER/out_2HDM.txt
 
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=32
 #SBATCH --time=400:00:00
 
@@ -11,5 +11,5 @@ module load openmpi410-gcc730
 module load gcc7.3.0
 
 
-mpic++ /home/athomas/DW_2HDM/mpi_evolution_2HDM_Z2_3D_scaling.cpp -o /home/athomas/DW_2HDM/Executables/mpi_evolution_2HDM_Z2_3D_scaling_1
-mpiexec -n 32 /home/athomas/DW_2HDM/Executables/mpi_evolution_2HDM_Z2_3D_scaling_1
+mpic++ /home/USER_DIR/mpi_evolution_2HDM_Z2.cpp -o /home/USER_DIR/mpi_evolution_2HDM_Z2
+mpiexec -n 64 /home/USER_DIR/mpi_evolution_2HDM_Z2
