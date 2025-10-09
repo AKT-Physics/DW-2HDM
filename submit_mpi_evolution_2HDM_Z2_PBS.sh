22e873429226ea42201eb097042b88eb9e048fe2
AKT-Physics <a.k.thomasson@gmail.com>
2025-10-09 08:35:58 +0100

    Update submit_mpi_evolution_2HDM_Z2_PBS.sh

--- submit_mpi_evolution_2HDM_Z2_PBS.sh
+++ submit_mpi_evolution_2HDM_Z2_PBS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=1:ppn=16
+#PBS -l nodes=2:ppn=32
 #PBS -l walltime=300:00:00
 #PBS -M NAME@student.manchester.ac.uk
 #PBS -m abe
@@ -11,5 +11,5 @@ module load gcc-9.3.0
 module load openmpi-1.10.4-withtm
 
 
-mpic++ /home/USER/DW_2HDM/mpi_evolution_2HDM_Z2.cpp -o /home/USER/DW_2HDM/Executables/mpi_evolution_2HDM_Z2
-mpiexec -n 16 /home/USER/DW_2HDM/Executables/mpi_evolution_2HDM_Z2
+mpic++ /home/USER_DIR/mpi_evolution_2HDM_Z2.cpp -o /home/USER_DIR/mpi_evolution_2HDM_Z2
+mpiexec -n 64 /home/USER_DIR/mpi_evolution_2HDM_Z2
